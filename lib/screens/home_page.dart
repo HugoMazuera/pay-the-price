557846ac56db43c43575a749707eeae31ca3d9b7
Hugo Mazuera <hmazuera89@outlook.com>
2022-11-29 15:55:25 -0700

    home page coming together, img assets laoding

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,44 +1,79 @@
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-void main() => runApp(const HomePage());
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  static const String _title = 'Pay the Price';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
-      ),
-    );
-  }
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({super.key});
+class _HomePageState extends State<HomePage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: InkWell(
-          splashColor: Colors.blue,
-          onTap: () {
-            debugPrint('Card tapped.');
-          },
-          child: const SizedBox(
-            width: 300,
-            height: 500,
-            child: Text('A card that can be tapped'),
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: const Color(0x00F1F4F8),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Stack(children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            height: MediaQuery.of(context).size.height * 1,
+            decoration: const BoxDecoration(
+              color: Color(0xFF1A54B8),
+            ),
           ),
-        ),
+          Align(
+            alignment: const AlignmentDirectional(1, 0),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 1,
+              decoration: const BoxDecoration(
+                color: Color(0xFFFC2641),
+              ),
+            ),
+          ),
+          const Align(
+            alignment: AlignmentDirectional(-0.01, -0.82),
+            child: Text("Pay The Price"),
+          ),
+          Align(
+            alignment: const AlignmentDirectional(1.01, 0.94),
+            child: InkWell(
+              onTap: () async {
+                await canLaunchUrlString('https://paythepricegame.com/');
+              },
+              child: Image.asset('images/ptp.png')
+            ),
+          ),
+          Align(
+            alignment: const AlignmentDirectional(0.67, 0.49),
+            child: InkWell(
+              onTap: () async {
+                await canLaunchUrlString('https://paythepricegame.com/');
+              },
+              child: const Text("Buy Here!"),
+            ),
+          ),
+          const Align(
+            alignment: AlignmentDirectional(-0.9, 0.91),
+            child: Text('Created By Santagato Studios'),
+          ),
+          const Align(
+            alignment: AlignmentDirectional(-0.89, 0.05),
+            child: Text('Play!'),
+          ),
+        ]),
       ),
+      // const Text('Settings'),
+      // Text('Cards'),
+
+      // Text('Trivia party game with hilarious consequences'),
     );
   }
 }
